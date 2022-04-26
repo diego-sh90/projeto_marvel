ab24be2896300a66e3ba28407c0da181d98ededb
diego.shirakura <diego.shirakura@gmail.com>
2022-04-25 22:17:53 -0300

    implementada a service generica

--- src/app/api/service.service.ts
+++ src/app/api/service.service.ts
@@ -7,5 +7,46 @@ import {Md5} from 'ts-md5/dist/md5';
   providedIn: 'root'
 })
 export class ServiceService {
-  
+    private publicKey ="";
+    private privateKey ="";
+
+    private host = "http://getway.marvel.com";
+
+    constructor(private http: HttpClient){};
+
+  public getDados(url: string, parameters: string){
+    let ts =" ";
+
+    return new Promise((ret) => {
+      this.http.get(this.host + url + '?ts=' + ts + '&hash' + this.getHash(ts) + parameters).subscribe((response) => {
+    
+        if(response){
+          ret(response);
+        }else{
+          ret(false);
+        }
+      })
+    })
+
+
+  }
+
+  private generateTs(){
+    return Math.floor(100000 + Math.random() * 900000);
+  }
+
+  private getHash(ts){
+    return Md5.hashStr(ts + this.privateKey + this.publicKey);
+  }
+
+  private getKeys(){
+    return new Promise((ret) => {
+      this.http.get('assets/keys.json').subscribe((keys:any) => {
+        this.publicKey = keys.public;
+        this.privateKey = keys.private;
+      
+        ret(true);
+      })
+    })
+  }
 }
